948078e34f54b8a791e38945614887e76f6fed54
vampirefan <vampirefanmail@gmail.com>
2024-11-22 16:56:01 +0800

    feat: 刪除收藏提示

--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -129,7 +129,7 @@ const App: React.FC = () => {
 
   return (
     <TypingContext.Provider value={{ state: state, dispatch }}>
-      <StarCard />
+      {/* <StarCard /> */}
       {state.isFinished && <DonateCard />}
       {state.isFinished && <ResultScreen />}
       <Layout>
