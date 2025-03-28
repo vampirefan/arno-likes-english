6c677b8734e9468142783f33c84c96513aff2662
vampirefan <vampirefanmail@gmail.com>
2025-03-28 08:30:02 +0800

    feat: 补充两个单词(yarn/shear)

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -10,7 +10,7 @@ const chinaExam: DictionaryResource[] = [
     category: '中国考试',
     tags: ['小学英语'],
     url: '/dicts/1-Arno_01.json',
-    length: 188,
+    length: 190,
     language: 'en',
     languageCategory: 'en',
   },
